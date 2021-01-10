0e0aaf5bb7524b373017101767bda8a91c3638b5
Attila Max Ruf <therufa@gmail.com>
2021-01-10 14:31:26 +0100

    add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: ['./src/**/*.svelte'],
+  purge: [
+    './public/**/*.html'
+  ],
   theme: {
-    extend: {}
+    fontFamily: {
+      title: ['Signika', 'sans-serif'],
+      body: ['"Open Sans"', 'sans-serif']
+    },
+    extend: {
+       colors: {
+         ...colors,
+         dark: {
+           bg: '#000C10',
+           subBg: '#001821',
+           comment: '#005673',
+           textDark: '#006182',
+           blue: '#0085B3',
+           textLight: '#2E9BC1',
+           textLighter: '#5DB1CF',
+           textLightest: '#74BCD6',
+           textSecondary: '#B32E00',
+           textSecLight: '#B38800',
+           text: '#fff'
+         },
+         transparent: 'transparent',
+         current: 'currentColor',
+       }
+    }
   },
   variants: {},
   plugins: []
