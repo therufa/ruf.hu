8f28a616ce98d8850059e8c39bfbe10db186924c
Attila Max Ruf <therufa@gmail.com>
2021-01-10 15:34:12 +0100

    remove src

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1 class="font-bold">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
